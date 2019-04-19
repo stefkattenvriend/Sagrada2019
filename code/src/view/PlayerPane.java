20dd472da13668027f59d0441dcedc379e404da3
StefvanOsch <sagj.vanosch@student.avans.nl>
2019-04-19 14:35:32 +0200

    JAAASSSSSSSS

--- code/src/view/PlayerPane.java
+++ code/src/view/PlayerPane.java
@@ -1,14 +1,30 @@
 package view;
 
-import javafx.scene.layout.FlowPane;
 
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+
 
 public class PlayerPane extends VBox{
 	
+	private FlowPane diceSection;
+	private RoundTrackPane roundTrackPane;
+	private DiceOfferPane diceOfferPane;
+	
 	public PlayerPane() {
+		super();
 		setBackground(controller.Main.PLAYERPANE); // aanduiding voor pane
+		setUp();
+	}
+	
+	private void setUp() {
 		setPaneSize();
+		diceSection = new FlowPane();
+		roundTrackPane = new RoundTrackPane();
+		diceOfferPane = new DiceOfferPane();
+
+		diceSection.getChildren().addAll(roundTrackPane, diceOfferPane);
+		getChildren().add(diceSection);
 	}
 	
 	private void setPaneSize() {
