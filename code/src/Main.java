b07d824df82c3fa8dd44221d17aaac6396c7c890
joeryvanegmond <joeryvanegmond@hotmail.com>
2019-04-19 10:50:40 +0200

    LAATSTE KANS

--- code/src/Main.java
+++ code/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Het spel Sagrada door groep N");
+		System.out.println("Het spel Sagrada >>voor<< groep N");
 
 	}
 
