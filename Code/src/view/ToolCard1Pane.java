3db9f4c173907705db5279b954e5542085d949d7
TjessWjest <49719362+TjessWjest@users.noreply.github.com>
2019-04-23 16:40:24 +0200

    toolcards begin
    opzettje maar ik snapte er niet zoveel van

---
+++ Code/src/view/ToolCard1Pane.java
@@ -0,0 +1,40 @@
+package view;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class ToolCard1Pane extends HBox {
+	private Button higher;
+	private Button lower;
+	public int chosen;
+	//dobbelsteen doorgeven via ToolCard
+	public ToolCard1Pane() {
+		higher = new Button("+1");
+		lower = new Button("-1");
+		higher.setOnMouseClicked(e -> higherClicked());
+		lower.setOnMouseClicked(e -> lowerClicked());
+		
+		//if(dobbelsteen.waarde==1){
+	//	lower disabled
+	//}
+		
+//		if(dobbelsteen.waarde ==6) {
+//			higher disabled
+//		}
+		
+		getChildren().addAll(higher, lower);
+	}
+
+	private void lowerClicked() {
+		chosen=1;
+		//pane weg
+	}
+
+	private void higherClicked() {
+		chosen=2;
+		//pane weg
+	
+	}
+}
