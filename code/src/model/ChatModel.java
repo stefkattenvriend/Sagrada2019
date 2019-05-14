cd85104f6a4df286974170332095377f76f7a15b
Milanovich123 <milanmastbergen@gmail.com>
2019-05-14 11:49:27 +0200

    chatmodel.2

--- code/src/model/ChatModel.java
+++ code/src/model/ChatModel.java
@@ -1,5 +1,31 @@
 package model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
+import databeest.DBChatCollector;
+
+//milan
 public class ChatModel {
 
+	//Instance Variables
+	private String message;
+	private ArrayList<String> messages = new ArrayList<>();
+	private DBChatCollector chatdatabase;
+	
+	//Constants
+	
+	//Constructor
+	public ChatModel(DBChatCollector chatdb) {
+		chatdatabase = chatdb;
+	}
+	
+	public void addMessage(String message) {
+		messages.add(message);
+	}
+	
+	public void pushChat(String message, int playerid, Date time) {
+		chatdatabase.pushChat(message, playerid, time);
+	}
+	
 }
