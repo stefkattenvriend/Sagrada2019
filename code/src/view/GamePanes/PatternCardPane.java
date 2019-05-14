0c218bfacd3213e37ebc1c34199607203bd58450
RensAspersAvans <rca.asper@student.avans.nl>
2019-05-14 15:49:57 +0200

    kleine fixes

--- code/src/view/GamePanes/PatternCardPane.java
+++ code/src/view/GamePanes/PatternCardPane.java
@@ -60,7 +60,7 @@ public class PatternCardPane extends Pane{//misschien hernoemen naar bord pane? 
 		}
 	}
 	
-	private void addPatternCard(int pcnumber) {//kijken waar we dit gaan gebruiken
+	private void addPatternCard(int pcnumber) {
 		for (int i = 1; i < 5; i++) {
 			for (int j = 1; j < 6; j++) {
 				double size = ((GamePane.windowMaxWidth / 3) / 5) - 1; 
