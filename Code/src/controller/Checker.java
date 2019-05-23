50996c534dff1e398a0b48b9718172e872e3ad15
jamester07 <jamisaueressig@gmail.com>
2019-05-23 10:15:46 +0200

    niks belangrijks

--- Code/src/controller/Checker.java
+++ Code/src/controller/Checker.java
@@ -6,6 +6,9 @@ public class Checker implements Runnable {
 	public void run() {
 		while(true) {
 			System.out.println("test");
+			
+			
+			
 			try {
 				Thread.sleep(10000);
 			} catch (InterruptedException e) {
