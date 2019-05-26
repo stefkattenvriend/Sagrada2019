308867246086787f05a720f7032bfb4683902736
TjessWjest <49719362+TjessWjest@users.noreply.github.com>
2019-05-26 11:30:49 +0200

    menuResultsPane
    klaargezet kan er mee verder als we spellen kunnen spelen

--- code/src/view/MenuPanes/MenuResultsPane.java
+++ code/src/view/MenuPanes/MenuResultsPane.java
@@ -1,19 +1,52 @@
 package view.MenuPanes;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import model.StatsModel;
+//Tesssss
+public class MenuResultsPane extends VBox{
+	private HBox won;
+	private HBox lost;
 
-public class MenuResultsPane extends Pane{
-	
 	public MenuResultsPane() {
+		
 		setPaneSize();
-//		setBackground(new Background(new BackgroundFill(Color.GREEN, null, null))); //tijdelijk
+		setAlignment(Pos.CENTER);
+		makePanes();
+		Label lbl1 = new Label("Jouw Statistieken: ");
+		lbl1.setFont(new Font(40));
+		getChildren().addAll(lbl1, won, lost);
+		
 	}
 
 	private void setPaneSize() {
 		setMinSize(MenuPane.paneWidth, MenuPane.windowMaxHeight / 3);
 		setMaxSize(MenuPane.paneWidth, MenuPane.windowMaxHeight / 3);
+	}
+	
+	private void makePanes() {
+		won = new HBox();
+		won.setAlignment(Pos.CENTER);
+		Label lbl2 = new Label("Aantal keer gewonnen: ");
+		Label lbl3 = new Label();
+		lbl3.setText("3"); //uit de database aantal halen van een int omzetten naar String en dan hierin zetten
+		lbl2.setFont(new Font(20));
+		lbl3.setFont(new Font(20));
+		won.getChildren().addAll(lbl2, lbl3);
+		
+		lost = new HBox();
+		lost.setAlignment(Pos.CENTER);
+		Label lbl4 = new Label("Aantal keer verloren: ");
+		Label lbl5 = new Label();
+		lbl5.setText("4");
+		lbl4.setFont(new Font(20));
+		lbl5.setFont(new Font(20));
+		lost.getChildren().addAll(lbl4, lbl5);
 	}
 }
