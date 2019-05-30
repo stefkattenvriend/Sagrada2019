35db469eecc27412fb1b182f8ceabdb8155023e1
RensAspersAvans <rca.asper@student.avans.nl>
2019-05-30 13:04:39 +0200

    Revert "Revert "Revert "Revert "Merge branch 'development' of https://github.com/stefkattenvriend/Sagrada2019 into development""""
    This reverts commit d12cd16ddfcbf52b7216d59fe9d104430e58430f.

--- Code/src/controller/PayStoneThread.java
+++ Code/src/controller/PayStoneThread.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class PayStoneThread {
+import model.PayStoneModel;
+import view.GamePanes.CardPane;
+
+public class PayStoneThread implements Runnable {
+	PayStoneController psc;
+	int cardId;
+	PayStoneModel psm;
+	
+	public PayStoneThread(PayStoneController psc, int cardId, CardPane cardPane) {
+		this.psc = psc;
+		this.cardId = cardId;
+		psm = new PayStoneModel();
+	}
+	
+	public PayStoneModel getModel() {
+		return psm;
+	}
+	
+	@Override
+	public void run() {
+		if(psm.getamount() != psc.getStonesOnCard(cardId)) {
+			psm.setamount(psc.getStonesOnCard(cardId));
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
