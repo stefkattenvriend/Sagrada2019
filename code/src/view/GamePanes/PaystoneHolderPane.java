513cf6e4684a61387db43d1660233f839be65834
joeryvanegmond <joeryvanegmond@hotmail.com>
2019-05-31 16:12:05 +0200

    paystoneholder design fix

--- code/src/view/GamePanes/PaystoneHolderPane.java
+++ code/src/view/GamePanes/PaystoneHolderPane.java
@@ -5,10 +5,12 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-public class PaystoneHolderPane extends Pane{
+public class PaystoneHolderPane extends VBox{
 	
 	private double personalAttributesHeight = 75;
 	private PayStoneController psc;
@@ -31,9 +33,14 @@ public class PaystoneHolderPane extends Pane{
 	}
 	
 	private void setPaystones(int amount) {
-		setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null))); //aanduiding van chatvak
+//		setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null))); //aanduiding van chatvak
 		Label text = new Label();
-		text.setText(Integer.toString(amount));
-		getChildren().addAll(text);
+		text.setText(" Betaalstenen:");
+		text.setFont(Font.font ("Verdana", FontWeight.BOLD, 15));
+		
+		Label textAmount = new Label();
+		textAmount.setText(" " + Integer.toString(amount));
+		textAmount.setFont(Font.font ("Verdana", FontWeight.BOLD, 30));
+		getChildren().addAll(text, textAmount);
 	}
 }
