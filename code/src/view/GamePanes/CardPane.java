069dc37864c5b23facc38f899edb0147ae3aae53
jamester07 <jamisaueressig@gmail.com>
2019-06-01 12:57:04 +0200

    paystone full upcdate

--- code/src/view/GamePanes/CardPane.java
+++ code/src/view/GamePanes/CardPane.java
@@ -11,34 +11,35 @@ import javafx.scene.layout.StackPane;
 public class CardPane extends StackPane{
 	private int cardNr;
 	private CardsController cc;
-	FlowPane ppsh = new FlowPane();
-	PayStoneThread ps;
 	GameController gc;
 	int stonesAmount = 0;
 	ImageView background;
+	boolean toolCard;
 	
 	public CardPane(ImageView background, boolean toolCard, CardsController cardsController, int cardNr, GameController gc) {
 		this.background = background;
 		this.gc = gc;
 		gc.addCardPane(this);
+		this.toolCard = toolCard;
 		cc = cardsController;
 		this.cardNr = cardNr;
 		setPrefSize((GamePane.windowMaxWidth / 6), (GamePane.windowMaxHeight - 40) / 3);
 		this.getChildren().addAll(this.background);
 		this.background.fitHeightProperty().bind(this.prefHeightProperty());
-		
+		FlowPane ppsh = new FlowPane();
 		ppsh.setPrefSize(this.getPrefWidth(), this.getPrefHeight());
 		this.getChildren().addAll(ppsh);
 		
 		if(toolCard) {
-			System.out.println("this should crash now!");
 			this.setOnMouseClicked(e -> cc.useCard(cardNr));
 		}
 	}
-	public void addPlayerPayStone() {
-		System.out.println("added a paystone");
-		PlayerPayStone pps = new PlayerPayStone(this);
-		ppsh.getChildren().addAll(pps);
+	public void addPlayerPayStone(FlowPane ppsh) {
+		 if(toolCard) {
+			System.out.println("added a paystone");
+			PlayerPayStone pps = new PlayerPayStone(this);
+			ppsh.getChildren().addAll(pps);
+		 }
 	}
 	
 	public int getCardNr() {
@@ -46,27 +47,20 @@ public class CardPane extends StackPane{
 	}
 	
 	public void refresh(int amount) {
+		System.out.println("refresh amount: " + amount);
 		stonesAmount = amount;
 		this.getChildren().clear();
 		this.getChildren().add(background);
+		FlowPane ppsh = new FlowPane();
+		ppsh.setPrefSize(this.getPrefWidth(), this.getPrefHeight());
 		this.getChildren().add(ppsh);
-		for(int i = 0; i < amount; i++) {
-			
-			this.addPlayerPayStone();
+		for(int i = 0; i < amount; i++) {	
+			this.addPlayerPayStone(ppsh);
 		}
 	}
 	
 	public void setStonesAmount(int amount) {
 		stonesAmount = amount;
-	}
-	
-
-	public void setPayStones(int amount) {
-		getChildren().clear();
-		for(int i = 0; i < amount; i++) {
-			this.addPlayerPayStone();
-		}
-		
 	}
 	
 	public int getStonesAmount() {
